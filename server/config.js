a42564ab8f4f95ccd22382eec7935ce1ea4cc2bc
Bhaumin Shah <bhaumin@gmail.com>
2019-09-05 19:51:18 +0530

    Fixing to set the node env constant in config correctly

--- server/config.js
+++ server/config.js
@@ -1,15 +1,8 @@
 const env = process.env;
 
-export const nodeEnv = env.NODE_ENV || "development";
-
-export const logStars = function(message) {
-  console.info("**********");
-  console.info(message);
-  console.info("**********");
-};
-
 export default {
   mongodbUri: "mongodb://localhost:27017",
   dbName: "jcnc",
-  port: env.PORT || 8080
+  port: env.PORT || 8080,
+  nodeEnv: env.NODE_ENV || "development"
 };
